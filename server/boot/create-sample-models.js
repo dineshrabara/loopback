275192d8a5cd54d734277431c98442558baf3c58
Dinesh Rabara <dinesh.rabara@gmail.com>
2015-04-23 17:43:25 +0530

    migrate user and roll

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,3 +1,4 @@
+//CoffeeShop
 module.exports = function(app) {
   app.dataSources.mysqlDs.automigrate('CoffeeShop', function(err) {
     if (err) throw err;
@@ -14,7 +15,7 @@ module.exports = function(app) {
   });
 };
 
-
+//category
 module.exports = function(app) {
   app.dataSources.mysqlDs.automigrate('category', function(err) {
     if (err) throw err;
@@ -31,7 +32,7 @@ module.exports = function(app) {
   });
 };
 
-
+//product
 module.exports = function(app) {
   app.dataSources.mysqlDs.automigrate('product', function(err) {
     if (err) throw err;
@@ -45,5 +46,51 @@ module.exports = function(app) {
 
       console.log('Models created: \n', products);
     });
+  });
+};
+
+//user
+module.exports = function(app) {
+  app.dataSources.mysqlDs.automigrate('User', function(err) {
+    if (err) throw err;
+
+    var User = app.models.User;
+    User.create({username:'dinesh',email: 'dinesh@sphererays.net', password: '123456'}, function(err, user) {
+      if (err) throw err;
+
+      console.log(user);
+    });
+  });
+};
+
+//AccessToken
+module.exports = function(app) {
+  app.dataSources.mysqlDs.automigrate('AccessToken', function(err) {
+    if (err) throw err;
+
+  });
+};
+
+//RoleMapping
+module.exports = function(app) {
+  app.dataSources.mysqlDs.automigrate('RoleMapping', function(err) {
+    if (err) throw err;
+        
+  });
+};
+
+//Role
+module.exports = function(app) {
+  app.dataSources.mysqlDs.automigrate('Role', function(err) {
+    if (err) throw err;
+        
+  });
+};
+
+//Role
+module.exports = function(app) {
+  app.dataSources.mysqlDs.automigrate('Role', function(err) {
+    if (err) throw err;
+        
   });
 };
